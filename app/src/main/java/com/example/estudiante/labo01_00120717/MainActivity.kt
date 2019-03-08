0a04b689473b8482b3433a7a29ef8206f2db6309
00120717 <00120717@uca.edu.sv>
2019-03-08 08:09:01 -0600

    Practica  1 + comentario

--- app/src/main/java/com/example/estudiante/labo01_00120717/MainActivity.kt
+++ app/src/main/java/com/example/estudiante/labo01_00120717/MainActivity.kt
@@ -2,7 +2,8 @@ package com.example.estudiante.labo01_00120717
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+/*Marvin Ramirez
+* 00120717*/
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
